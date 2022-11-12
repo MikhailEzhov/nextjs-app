05fcd06ab087150b8977d89618e07accd548f183
MikhailEzhov <Hammer.perm@gmail.com>
2022-11-12 19:48:37 +0500

    feat: added progressbar when loading pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import "../styles/globals.scss";
+import NextNprogress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNprogress
+        color="white"
+        startPosition="0.3"
+        stopDelayMs="200"
+        height="2"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
