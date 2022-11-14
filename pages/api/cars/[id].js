40e8d952297ab249ee4588613a0069a1257e163a
MikhailEzhov <Hammer.perm@gmail.com>
2022-11-14 15:58:42 +0500

    feat: added internal api methods

--- pages/api/cars/[id].js
+++ pages/api/cars/[id].js
@@ -5,13 +5,22 @@ export default function handler(request, response) {
 
   if (method === "GET") {
     const { id } = request.query;
-
-    const car = cars.find((car) => car.id.toString() === id);
-
-    if (!car) {
-      return response.status(400).json("not found");
-    }
-
+    const car = cars.find((car) => car.id === parseInt(id));
     return response.status(200).json(car);
+  }
+
+  if (method === "PUT") {
+    const { body } = request;
+    const index = cars.findIndex((car) => car.id === parseInt(body.id));
+    const newCars = [...cars, (cars[index] = body)];
+    return response.status(200).json(newCars);
+  }
+
+  if (method === "DELETE") {
+    const { id } = request.query;
+    const deletedCar = cars.find((car) => car.id === parseInt(id));
+    const index = cars.findIndex((car) => car.id === parseInt(id));
+    cars.splice(index, 1);
+    return response.status(200).json(deletedCar);
   }
 }
--- pages/api/cars/index.js
+++ pages/api/cars/index.js
@@ -9,7 +9,7 @@ export default function handler(request, response) {
 
   if (method === "POST") {
     const { body } = request;
-    cars.push({ ...body, id: cars.length + 1 });
-    return response.status(200).json(cars);
+    const newCars = cars.push(body);
+    return response.status(200).json(newCars);
   }
 }
